2036da06676f81ff8008d611bb675536af698346
Dinuka Liyanage <infodinukalk@gmail.com>
2025-09-23 02:04:32 +0530

    initial commit

---
+++ src/main/java/lk/school/elite_driving/controller/WelcomePageController.java
@@ -0,0 +1,34 @@
+package lk.school.elite_driving.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WelcomePageController {
+    public AnchorPane rootNode;
+
+    public void btnReceptionistOnAction(ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/lk/school/elite_driving/view/receptionist/ReceptionistLogin.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(anchorPane);
+        stage.setTitle("Receptionist Login");
+        stage.setScene(scene);
+        stage.show();
+        Stage prevStage = (Stage) rootNode.getScene().getWindow();
+        prevStage.close();
+    }
+    public void btnAdminOnAction(ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/lk/school/elite_driving/view/admin/AdminLogin.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(anchorPane);
+        stage.setTitle("Admin Login");
+        stage.setScene(scene);
+        stage.show();
+        Stage prevStage = (Stage) rootNode.getScene().getWindow();
+        prevStage.close();
+    }
+}
